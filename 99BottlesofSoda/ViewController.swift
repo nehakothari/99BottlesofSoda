d47addbf788d879292edf68475026e23b3ae4e5b
Neha Kothari <sneha.kothari@gmail.com>
2014-10-01 18:05:20 -0700

    First revision

--- 99BottlesofSoda/ViewController.swift
+++ 99BottlesofSoda/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for var bottlesofSoda = 100; bottlesofSoda >= 0; bottlesofSoda--
+        {
+            if bottlesofSoda > 0 {
+                println("\(bottlesofSoda) bottles of soda")
+            }
+            else {
+                println("No more bottles of soda on the wall.")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
